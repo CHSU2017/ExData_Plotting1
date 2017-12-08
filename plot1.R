dca06e0060362ffd755ae30f8bcbb3981d487764
CHSU2017 <32804191+CHSU2017@users.noreply.github.com>
2017-12-08 12:38:40 -0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -13,7 +13,7 @@ powerF <- powerF[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
 png("plot1.png", width=480, height=480)
 
 ## Plot 1
-hist(powerDT[, Global_active_power], main="Global Active Power", 
+hist(powerF[, Global_active_power], main="Global Active Power", 
      xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 
 dev.off()
